d7e39e07a8a8f02e5879e6c8c056f698ccd16346
Saad Al Muttakee <muttakee9@gmail.com>
2024-08-25 23:40:03 +0600

    added marker packages and styling

--- web_app/hazard-reporting-map/pages/_app.tsx
+++ web_app/hazard-reporting-map/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
