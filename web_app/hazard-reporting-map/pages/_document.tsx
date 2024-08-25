a9f82864bf4a6ad02189d88bf9a50528ef681c81
Saad Al Muttakee <muttakee9@gmail.com>
2024-08-26 00:28:50 +0600

    added navbar

--- web_app/hazard-reporting-map/pages/_document.tsx
+++ web_app/hazard-reporting-map/pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <nav className="bg-blue-600 text-white p-4 h-[8vh] z-100 flex items-center">
+          <h1 className="text-xl font-bold">Hazard Reporting 2024</h1>
+        </nav>
         <Main />
         <NextScript />
       </body>
