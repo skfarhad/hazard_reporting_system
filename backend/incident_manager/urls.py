76d7eac4c62c3578eea55be9dfb570f3abde9324
sagorluc <mdsagorluc@gmail.com>
2024-08-31 01:29:52 +0600

    added image field in incident model and api view

--- backend/incident_manager/urls.py
+++ backend/incident_manager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .api import IncidentCreateView
+from .api import IncidentCreateView, IncidentApiView
 
 urlpatterns = [
     path("", IncidentCreateView.as_view(), name="incident-create"),
+    path("view/", IncidentApiView.as_view(), name="incident-view"),
+    path("view/<int:id>", IncidentApiView.as_view(), name="incident-single"),
 ]
