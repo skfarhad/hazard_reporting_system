58cbf3280b29c06c89ced75b833d0cf80c0292b2
aimun nahar <aimunnaharr@gmail.com>
2024-09-04 11:16:50 +0600

    added reusable components for similar fields

--- web_app/hazard-reporting-map/components/modals/volunteers/AddVolunteer.tsx
+++ web_app/hazard-reporting-map/components/modals/volunteers/AddVolunteer.tsx
@@ -21,7 +21,7 @@ import {
 } from '@/components/ui/drawer';
 import Container from '@/components/layouts/Container';
 import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
+import { Input, InputProps } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
 import mapboxgl from 'mapbox-gl';
 import { SearchBox } from '@mapbox/search-js-react';
@@ -38,11 +38,122 @@ import { Textarea } from '@/components/ui/textarea';
 import { MarkerData } from '@/types/MarkerData';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { toast } from 'sonner';
+import { cn } from '@/libs/utils';
 
 const accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
 
 mapboxgl.accessToken = accessToken;
 const SafeSearchBox = SearchBox as React.ComponentType<any>;
+
+type TInputProps = {
+  disabled: boolean;
+  label: string;
+  inputClassName?: string;
+  labelClassName?: string;
+  className?: string;
+} & InputProps;
+const WiderInput = ({
+  disabled,
+  label,
+  name,
+  inputClassName,
+  labelClassName,
+  className,
+  ...inputProps
+}: TInputProps) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-col lg:flex-row lg:items-center w-full  gap-2 2xl:gap-24 lg:gap-18',
+        className
+      )}
+    >
+      <Label id={name} className={cn('w-[160px]  text-nowrap', labelClassName)}>
+        {label}
+      </Label>
+      <Input
+        id={name}
+        name={name}
+        className={cn(' w-full border flex-1', inputClassName)}
+        {...inputProps}
+        disabled={disabled}
+      />
+    </div>
+  );
+};
+
+const SmallInput = ({
+  disabled,
+  label,
+  name,
+  inputClassName,
+  labelClassName,
+  className,
+  ...inputProps
+}: TInputProps) => {
+  return (
+    <div
+      className={cn(
+        'flex flex-col md:flex-row justify-between md:items-center w-full',
+        className
+      )}
+    >
+      <Label className={cn('w-[160px]', labelClassName)}>{label}</Label>
+      <Input
+        id={name}
+        name={name}
+        className={cn(' w-full border flex-1', inputClassName)}
+        {...inputProps}
+        disabled={disabled}
+      />
+    </div>
+  );
+};
+interface DropDownOption<T> {
+  value: T;
+  label: string; // Use string for label to handle text display
+}
+
+interface TDropDown<T> {
+  value: T;
+  onChange: (value: string) => void;
+  options: DropDownOption<T>[];
+  label: string;
+  name: string;
+  placeholder: string;
+  icon?: ReactNode;
+}
+
+const DropDown = ({
+  value,
+  onChange,
+  options,
+  label,
+  name,
+  placeholder,
+  icon,
+}: TDropDown<string>) => {
+  return (
+    <div className="flex items-center gap-20 w-full lg:w-1/2">
+      <Label className="">{label}</Label>
+      <Select value={value} name={name} onValueChange={onChange}>
+        <SelectTrigger className="w-full bg-[#F3F6F8]">
+          {icon ? icon : null}
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((itm, i) => {
+            return (
+              <SelectItem key={i} value={itm.value}>
+                {itm.label}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
 
 type TDrawerProps = {
   open: boolean;
@@ -154,8 +265,8 @@ export default function AddVolunteer({
       <DrawerTrigger asChild>{children}</DrawerTrigger>
       <DrawerContent>
         <div className="h-[90vh] overflow-y-auto">
-          <Container className="mx-auto w-full ">
-            <DrawerHeader className=" w-full">
+          <Container className="max-w-[1220px] w-full ">
+            <DrawerHeader className=" w-full  px-0">
               <DrawerTitle className="text-left font-semibold">
                 Add Volunteer
               </DrawerTitle>
@@ -179,157 +290,123 @@ export default function AddVolunteer({
               </div>
             </DrawerHeader>
             <div className=" p-0 pb-0">
-              <div className="flex md:flex-row flex-col md:justify-between">
+              <div className="flex md:flex-row flex-col gap-4 lg:gap-8 md:justify-between">
                 {/* inputs section */}
                 <div className="md:w-1/2 w-full  p-3 flex flex-col gap-4">
                   {/* name, number, org */}
                   <div className="w-full flex flex-col gap-5 ">
-                    <div className="flex flex-col lg:flex-row lg:items-center w-full  gap-2 2xl:gap-24 lg:gap-18">
-                      <Label className="w-[160px]  text-nowrap">
-                        Volunteer name
-                      </Label>
-                      <Input
-                        className=" w-full border"
-                        placeholder="Name of the Volunteer"
-                        name="volunteerName"
-                        onChange={handleInputChange}
-                        value={formInfo.volunteerName}
-                        disabled={isFormLoading}
-                      />
-                    </div>
-                    <div className="flex flex-col lg:flex-row lg:items-center w-full  gap-2 2xl:gap-24 lg:gap-18">
-                      <Label className="w-[160px]  text-nowrap">
-                        Contact number
-                      </Label>
-                      <Input
-                        className=" w-full border"
-                        placeholder="+8801800xxxxxxx"
-                        name="contactNumber"
-                        onChange={handleInputChange}
-                        value={formInfo.contactNumber}
-                        disabled={isFormLoading}
-                      />
-                    </div>
-                    <div className="flex flex-col lg:flex-row lg:items-center w-full  gap-2 2xl:gap-24 lg:gap-18">
-                      <Label className="w-[160px]  text-nowrap">
-                        Organization
-                      </Label>
-                      <Input
-                        className=" w-full border"
-                        placeholder="Affiliated Organization"
-                        name="organization"
-                        onChange={handleInputChange}
-                        value={formInfo.organization}
-                        disabled={isFormLoading}
-                      />
-                    </div>
-                  </div>
-                  <Separator />
-                  {/* status */}
-                  <div className="flex items-center gap-20 w-full lg:w-1/2">
-                    <Label className="">Status</Label>
-                    <Select
-                      value={'active'}
-                      name="status"
-                      onValueChange={(value) =>
-                        handleDropDownChange(value, 'status')
-                      }
-                    >
-                      <SelectTrigger className="w-full bg-[#F3F6F8]">
-                        <SelectValue placeholder="Status" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="active">Active</SelectItem>
-                        <SelectItem value="inactive">Inactive</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                  {/* district  , thana*/}
-                  <div className="flex md:gap-6 gap-3 lg:flex-row flex-col">
-                    <div className="flex items-center gap-20 w-full">
-                      <Label className="">District</Label>
-                      <Select
-                        name="district"
-                        value={'Feni'}
-                        onValueChange={(value) =>
-                          handleDropDownChange(value, 'district')
-                        }
-                      >
-                        <SelectTrigger className="w-full bg-[#F3F6F8]">
-                          <Image
-                            src={icons.Location2}
-                            alt="location icon"
-                            height={16}
-                            width={16}
-                          />{' '}
-                          <SelectValue placeholder="Feni" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value="Feni">Feni</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </div>
-                    <div className="flex items-center gap-20 w-full">
-                      <Label className="">Thana</Label>
-                      <Select
-                        name="thana"
-                        value={'parshuram'}
-                        onValueChange={(value) =>
-                          handleDropDownChange(value, 'thana')
-                        }
-                      >
-                        <SelectTrigger className="w-full bg-[#F3F6F8]">
-                          <Image
-                            src={icons.Location2}
-                            alt="location icon"
-                            height={16}
-                            width={16}
-                          />{' '}
-                          <SelectValue placeholder="" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value="parshuram">Parshuram</SelectItem>
-                          <SelectItem value="inactive">Inactive</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </div>
-                  </div>
-                  {/* address */}
-                  <div className="flex flex-col md:flex-row md:items-center w-full  gap-2 md:gap-24">
-                    <Label className="w-[160px]  text-nowrap">Address</Label>
-                    <Input
-                      name="address"
-                      className=" w-full border"
-                      placeholder="Area/Union/Ward details"
+                    <WiderInput
+                      name="volunteerName"
+                      label="Volunteer Name"
+                      placeholder="Name of the Volunteer"
                       onChange={handleInputChange}
-                      value={formInfo.address}
+                      value={formInfo.volunteerName}
+                      disabled={isFormLoading}
+                    />
+                    <WiderInput
+                      placeholder="+8801800xxxxxxx"
+                      name="contactNumber"
+                      onChange={handleInputChange}
+                      value={formInfo.contactNumber}
+                      disabled={isFormLoading}
+                      label="Contact number"
+                    />
+
+                    <WiderInput
+                      label="Organization"
+                      placeholder="Affiliated Organization"
+                      name="organization"
+                      onChange={handleInputChange}
+                      value={formInfo.organization}
                       disabled={isFormLoading}
                     />
                   </div>
+                  <Separator />
+                  {/* status */}
+
+                  <DropDown
+                    value={formInfo.status}
+                    name="status"
+                    options={[
+                      { value: 'active', label: 'Active' },
+                      { value: 'inactive', label: 'Inactive' },
+                    ]}
+                    onChange={(value) => handleDropDownChange(value, 'status')}
+                    label="Status"
+                    placeholder="Status"
+                  />
+                  {/* district  , thana*/}
+                  <div className="flex md:gap-6 gap-3 lg:flex-row flex-col">
+                    <DropDown
+                      label="District"
+                      name="district"
+                      onChange={(value) =>
+                        handleDropDownChange(value, 'district')
+                      }
+                      value={formInfo.district}
+                      placeholder="District"
+                      options={[
+                        { value: 'feni', label: 'Feni' },
+                        { value: 'chittagong', label: 'Chittagong' },
+                      ]}
+                      icon={
+                        <Image
+                          src={icons.Location2}
+                          alt="location icon"
+                          height={16}
+                          width={16}
+                        />
+                      }
+                    />
+
+                    <DropDown
+                      label="Thana"
+                      name="thana"
+                      onChange={(value) => handleDropDownChange(value, 'thana')}
+                      value={formInfo.thana}
+                      placeholder="Thana"
+                      options={[
+                        { value: 'parshuram', label: 'Parshuram' },
+                        { value: 'sadarghat', label: 'Sadarghat' },
+                      ]}
+                      icon={
+                        <Image
+                          src={icons.Location2}
+                          alt="location icon"
+                          height={16}
+                          width={16}
+                        />
+                      }
+                    />
+                  </div>
+
+                  {/* address */}
+                  <WiderInput
+                    name="address"
+                    placeholder="Area/Union/Ward details"
+                    onChange={handleInputChange}
+                    value={formInfo.address}
+                    disabled={isFormLoading}
+                    label="Address"
+                  />
                   {/* latitude and longitude */}
                   <div className="flex gap-3 md:gap-6 flex-col lg:flex-row">
-                    <div className="flex flex-col md:flex-row justify-between md:items-center w-full">
-                      <Label className=" w-[160px]">Latitude</Label>
-                      <Input
-                        className=" w-full border"
-                        placeholder="23.93933"
-                        name="lat"
-                        onChange={handleInputChange}
-                        value={formInfo.lat}
-                        disabled={isFormLoading}
-                      />
-                    </div>
-                    <div className="flex flex-col md:flex-row justify-between md:items-center w-full">
-                      <Label className="w-[160px] ">Longitude</Label>
-                      <Input
-                        className=" w-full border"
-                        placeholder="91.36479"
-                        name="lon"
-                        onChange={handleInputChange}
-                        value={formInfo.lon}
-                        disabled={isFormLoading}
-                      />
-                    </div>
+                    <SmallInput
+                      placeholder="23.93933"
+                      name="lat"
+                      onChange={handleInputChange}
+                      value={formInfo.lat}
+                      disabled={isFormLoading}
+                      label="Latitude"
+                    />
+                    <SmallInput
+                      label={'Longitude'}
+                      placeholder="91.36479"
+                      name="lon"
+                      onChange={handleInputChange}
+                      value={formInfo.lon}
+                      disabled={isFormLoading}
+                    />
                   </div>
 
                   {/* biography */}
