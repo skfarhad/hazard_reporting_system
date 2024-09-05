9be3f110ae580f47d0a99330e291f59674311ca8
dg1223 <alavi1223@hotmail.com>
2024-09-05 01:01:43 -0400

    feat/data: fix indentation according to autopep8 guidelines

--- data/normalize_db.sql
+++ data/normalize_db.sql
@@ -21,11 +21,13 @@ ALTER TABLE adm1_division
 -- 2. Add unique constraint and set foreign key constraint
 ALTER TABLE adm1_division
     ADD CONSTRAINT unique_admin1_pcode_fkey UNIQUE (admin1_pcode_fkey),
-    ADD CONSTRAINT fk_admin0_pcode FOREIGN KEY (admin0_pcode_fkey) REFERENCES adm0_country(admin0_pcode);
+    ADD CONSTRAINT fk_admin0_pcode FOREIGN KEY (admin0_pcode_fkey) 
+        REFERENCES adm0_country(admin0_pcode);
 
--- 3. Update the new column by removing 'BD' prefix and converting the value to SMALLINT
+-- 3. Update the new column by removing 'BD' prefix and converting to SMALLINT
 UPDATE adm1_division
-SET admin1_pcode_fkey = CAST(SUBSTRING(admin1_pcode FROM 3 FOR CHAR_LENGTH(admin1_pcode) - 2) AS SMALLINT);
+SET admin1_pcode_fkey = CAST(SUBSTRING(
+            admin1_pcode FROM 3 FOR CHAR_LENGTH(admin1_pcode) - 2) AS SMALLINT);
 
 -- 4. Drop redundant column
 ALTER TABLE adm1_division
@@ -43,13 +45,17 @@ ALTER TABLE adm2_district
 -- 2. Add unique constraint and set foreign key constraints
 ALTER TABLE adm2_district
     ADD CONSTRAINT unique_admin2_pcode_fkey UNIQUE (admin2_pcode_fkey),
-    ADD CONSTRAINT fk_admin1_pcode FOREIGN KEY (admin1_pcode_fkey) REFERENCES adm1_division(admin1_pcode_fkey),
-    ADD CONSTRAINT fk_admin0_pcode FOREIGN KEY (admin0_pcode_fkey) REFERENCES adm0_country(admin0_pcode);
+    ADD CONSTRAINT fk_admin1_pcode FOREIGN KEY (admin1_pcode_fkey) 
+                                    REFERENCES adm1_division(admin1_pcode_fkey),
+    ADD CONSTRAINT fk_admin0_pcode FOREIGN KEY (admin0_pcode_fkey) 
+                                    REFERENCES adm0_country(admin0_pcode);
 
--- 3. Update the new column by removing 'BD' prefix and converting the value to SMALLINT
+-- 3. Update the new column by removing 'BD' prefix and converting to SMALLINT
 UPDATE adm2_district
-SET admin2_pcode_fkey = CAST(SUBSTRING(admin2_pcode FROM 3 FOR CHAR_LENGTH(admin2_pcode) - 2) AS SMALLINT),
-    admin1_pcode_fkey = CAST(SUBSTRING(admin1_pcode FROM 3 FOR CHAR_LENGTH(admin1_pcode) - 2) AS SMALLINT);
+SET admin2_pcode_fkey = CAST(SUBSTRING(
+            admin2_pcode FROM 3 FOR CHAR_LENGTH(admin2_pcode) - 2) AS SMALLINT),
+    admin1_pcode_fkey = CAST(SUBSTRING(
+            admin1_pcode FROM 3 FOR CHAR_LENGTH(admin1_pcode) - 2) AS SMALLINT);
 
 -- 4. Drop redundant columns
 ALTER TABLE adm2_district
@@ -70,15 +76,21 @@ ALTER TABLE adm3_upazila
 -- 2. Add unique constraint and set foreign key constraints
 ALTER TABLE adm3_upazila
     ADD CONSTRAINT unique_admin3_pcode_fkey UNIQUE (admin3_pcode_fkey),
-    ADD CONSTRAINT fk_admin2_pcode FOREIGN KEY (admin2_pcode_fkey) REFERENCES adm2_district(admin2_pcode_fkey),
-    ADD CONSTRAINT fk_admin1_pcode FOREIGN KEY (admin1_pcode_fkey) REFERENCES adm1_division(admin1_pcode_fkey),
-    ADD CONSTRAINT fk_admin0_pcode FOREIGN KEY (admin0_pcode_fkey) REFERENCES adm0_country(admin0_pcode);
+    ADD CONSTRAINT fk_admin2_pcode FOREIGN KEY (admin2_pcode_fkey) 
+                                    REFERENCES adm2_district(admin2_pcode_fkey),
+    ADD CONSTRAINT fk_admin1_pcode FOREIGN KEY (admin1_pcode_fkey) 
+                                    REFERENCES adm1_division(admin1_pcode_fkey),
+    ADD CONSTRAINT fk_admin0_pcode FOREIGN KEY (admin0_pcode_fkey) 
+                                    REFERENCES adm0_country(admin0_pcode);
 
--- 3. Update the new column by removing 'BD' prefix and converting the value to INTEGER
+-- 3. Update the new column by removing 'BD' prefix and converting to INTEGER
 UPDATE adm3_upazila
-SET admin3_pcode_fkey = CAST(SUBSTRING(admin3_pcode FROM 3 FOR CHAR_LENGTH(admin3_pcode) - 2) AS INTEGER),
-    admin2_pcode_fkey = CAST(SUBSTRING(admin2_pcode FROM 3 FOR CHAR_LENGTH(admin2_pcode) - 2) AS SMALLINT),
-    admin1_pcode_fkey = CAST(SUBSTRING(admin1_pcode FROM 3 FOR CHAR_LENGTH(admin1_pcode) - 2) AS SMALLINT);
+SET admin3_pcode_fkey = CAST(SUBSTRING(
+            admin3_pcode FROM 3 FOR CHAR_LENGTH(admin3_pcode) - 2) AS INTEGER),
+    admin2_pcode_fkey = CAST(SUBSTRING(
+            admin2_pcode FROM 3 FOR CHAR_LENGTH(admin2_pcode) - 2) AS SMALLINT),
+    admin1_pcode_fkey = CAST(SUBSTRING(
+            admin1_pcode FROM 3 FOR CHAR_LENGTH(admin1_pcode) - 2) AS SMALLINT);
 
 -- 4. Drop redundant columns
 ALTER TABLE adm3_upazila
@@ -100,16 +112,23 @@ ALTER TABLE adm4_thana_union
 
 -- 2. Add unique constraint and set foreign key constraints
 ALTER TABLE adm4_thana_union
-    ADD CONSTRAINT fk_admin3_pcode FOREIGN KEY (admin3_pcode_fkey) REFERENCES adm3_upazila(admin3_pcode_fkey),
-    ADD CONSTRAINT fk_admin2_pcode FOREIGN KEY (admin2_pcode_fkey) REFERENCES adm2_district(admin2_pcode_fkey),
-    ADD CONSTRAINT fk_admin1_pcode FOREIGN KEY (admin1_pcode_fkey) REFERENCES adm1_division(admin1_pcode_fkey),
-    ADD CONSTRAINT fk_admin0_pcode FOREIGN KEY (admin0_pcode_fkey) REFERENCES adm0_country(admin0_pcode);
+    ADD CONSTRAINT fk_admin3_pcode FOREIGN KEY (admin3_pcode_fkey) 
+                                    REFERENCES adm3_upazila(admin3_pcode_fkey),
+    ADD CONSTRAINT fk_admin2_pcode FOREIGN KEY (admin2_pcode_fkey) 
+                                    REFERENCES adm2_district(admin2_pcode_fkey),
+    ADD CONSTRAINT fk_admin1_pcode FOREIGN KEY (admin1_pcode_fkey) 
+                                    REFERENCES adm1_division(admin1_pcode_fkey),
+    ADD CONSTRAINT fk_admin0_pcode FOREIGN KEY (admin0_pcode_fkey) 
+                                    REFERENCES adm0_country(admin0_pcode);
 
--- 3. Update the new column by removing 'BD' prefix and converting the value to INTEGER
+-- 3. Update the new column by removing 'BD' prefix and converting to INTEGER
 UPDATE adm4_thana_union
-SET admin3_pcode_fkey = CAST(SUBSTRING(admin3_pcode FROM 3 FOR CHAR_LENGTH(admin3_pcode) - 2) AS INTEGER),
-    admin2_pcode_fkey = CAST(SUBSTRING(admin2_pcode FROM 3 FOR CHAR_LENGTH(admin2_pcode) - 2) AS SMALLINT),
-    admin1_pcode_fkey = CAST(SUBSTRING(admin1_pcode FROM 3 FOR CHAR_LENGTH(admin1_pcode) - 2) AS SMALLINT);
+SET admin3_pcode_fkey = CAST(SUBSTRING(
+            admin3_pcode FROM 3 FOR CHAR_LENGTH(admin3_pcode) - 2) AS INTEGER),
+    admin2_pcode_fkey = CAST(SUBSTRING(
+            admin2_pcode FROM 3 FOR CHAR_LENGTH(admin2_pcode) - 2) AS SMALLINT),
+    admin1_pcode_fkey = CAST(SUBSTRING(
+            admin1_pcode FROM 3 FOR CHAR_LENGTH(admin1_pcode) - 2) AS SMALLINT);
 
 -- 4. Drop the old pcode column
 ALTER TABLE adm4_thana_union
